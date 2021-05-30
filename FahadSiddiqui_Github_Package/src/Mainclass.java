02fcfdaafc044f0bf59c8495769ad26a835db26d
FadFlex <fsiddiqui107@gmail.com>
2021-05-30 08:54:42 +0500

    work complete

---
+++ FahadSiddiqui_Github_Package/src/Mainclass.java
@@ -0,0 +1,12 @@
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is main class
+		
+//Muhammad Fahad Siddiqui
+		//2019-SE-206
+	}
+
+}
